ee1e659984702de2417f77cc5e37265ae76b9b70
Brandon Bremen <bbremen@gmail.com>
2023-01-19 07:16:13 -0800

    add blender model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BlenderbotForConditionalGeneration
 import torch
 
 
@@ -37,6 +37,48 @@ class CausalModel:
         return self.model.generate(input_ids,
                                    pad_token_id=self.tokenizer.eos_token_id,
                                    do_sample=True,
+                                   temperature=temperature,
+                                   max_new_tokens=max_new_tokens,
+                                   )
+
+    def generate_text(self, prompt, temperature=None, max_new_tokens=None):
+        """ The whole flow. Reads a prompt and outputs generated text. """
+        if not temperature:
+            temperature = 0.9
+        if not max_new_tokens:
+            max_new_tokens = 128
+        input_ids = self.tokenize(prompt)
+        gen_tokens = self.generate(input_ids, temperature, max_new_tokens)
+        return self.tokenizer.batch_decode(gen_tokens)[0]
+
+
+class BlenderbotModel:
+    def __init__(self, model_name="facebook/blenderbot-400M-distill", cache_dir=None):
+        if not torch.cuda.is_available():
+            raise RuntimeError("torch cuda support not installed. See pytorch.org for installation details.")
+
+        self.device = torch.device('cuda:0')
+        dtype = torch.float32
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir,
+                                                       low_memory=True)
+        model = BlenderbotForConditionalGeneration.from_pretrained(model_name,
+                                                                   cache_dir=cache_dir,
+                                                                   torch_dtype=dtype,
+                                                                   encoder_no_repeat_ngram_size=0,
+                                                                   low_cpu_mem_usage=True)
+
+        self.model = model.to(self.device, dtype=dtype)
+
+    def tokenize(self, prompt):
+        return self.tokenizer(prompt, return_tensors="pt").input_ids.to(self.device)
+
+    def generate(self, input_ids, temperature=None, max_new_tokens=None):
+        if not temperature:
+            temperature = 0.9
+        if not max_new_tokens:
+            max_new_tokens = 128
+        return self.model.generate(input_ids,
                                    temperature=temperature,
                                    max_new_tokens=max_new_tokens,
                                    )
